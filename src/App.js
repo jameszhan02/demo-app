1ea90bd34e307c7217c863664b6ebf4df933fb89
Sherry <bcereal@students.cs.ubc.ca>
2021-08-06 19:53:13 -0700

    set up express and prints hello world

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: " "};
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/helloworld")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res}))
+      .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return(
+      <div className="app">
+        <div className = "hello-world"> 
+            {this.state.apiResponse}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
